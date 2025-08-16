7cdbe56865c1cd219f0fc6f28be6ce063a520811
tamovn <tienoivn.23@gmail.com>
2025-08-16 16:03:01 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      __GEMINI_API_KEY__: JSON.stringify(env.VITE_GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    base: './' // 👈 để tránh lỗi đường dẫn khi deploy
+  };
 });
