7c3894fa503d2dadd4ee5554461ecc1d202efdfa
tamovn <tienoivn.23@gmail.com>
2025-08-17 13:35:37 +0700

    okok

--- api-proxy/server.js
+++ api-proxy/server.js
@@ -9,13 +9,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.VITE_GEMINI_API_KEY);
 
 app.post('/api/generate', async (req, res) => {
-  const { prompt } = req.body;
+  const data = req.body;
   try {
-    const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
-    const result = await model.generateContent(prompt);
+    if (!data) {
+      return res.status(400).json({ error: "Thiếu prompt trong request body" });
+    }
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+    // ✅ Tạo prompt từ JSON (tùy biến theo mục đích của bạn)
+    const customPromt = `
+      trở thành 1 chuyên gia tư vấn, dựa vào tên sản phẩm, mô tả ngắn, mô tả chi tiết hãy đánh giá sản phẩm này có thực sự phù hợp với nhau cầu của khách hàng hay không, và khuyến khích họ mua sản phẩm:
+
+      Tên sản phẩm: ${data.name}
+      Mô tả ngắn: ${data.description}
+      Giá: ${data.price}
+      Link: ${data.link}
+      Hình ảnh: ${data.image}
+      Loại: ${data.type}
+      Tình trạng: ${data.status}
+      Mô tả chi tiết: ${data.description_detail}
+      Nút CTA: ${data.button_text}
+    `;
+    const formPromt = {
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: customPromt }],
+        },
+      ],
+    }
+    const result = await model.generateContent(formPromt);
     const text = result.response.text();
     res.json({ result: text });
   } catch (err) {
